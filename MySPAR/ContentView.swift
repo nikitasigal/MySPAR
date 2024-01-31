e07d9b17b28cb6ebdea90c2d537c4b584cb717ec
Nikita Sigal <nikitasigal@icloud.com>
2024-01-31 13:13:41 +0300

    Initial Commit

---
+++ MySPAR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySPAR
+//
+//  Created by Никита Сигал on 31.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySPAR/MySPARApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySPARApp.swift
+//  MySPAR
+//
+//  Created by Никита Сигал on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySPARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
